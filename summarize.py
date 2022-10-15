e775ae533cc540e75ecfd62eacc5e926678c869c
Hriday Algh <hridayalgh@gmail.com>
2022-10-15 15:32:14 -0400

    Re add summarize.py lol

---
+++ summarize.py
@@ -0,0 +1,34 @@
+import cohere
+import whisper
+import os
+api_key = 'FRyzo2xU7j2Cp9sPgpSHTafsBsuI3X4UnNmBmDZO'
+co = cohere.Client(api_key)
+model = whisper.load_model("base")
+
+def to_transcript(filename):
+    result = model.transcribe(filename.strip())
+    return result["text"]
+
+def do_cohere(prompt):
+    n_generations = 4
+    prediction = co.generate(
+        model='large',
+        prompt=prompt,
+        return_likelihoods = 'GENERATION',
+        stop_sequences=['"'],
+        max_tokens = min(512, max(40, len(prompt)//10)),
+        temperature=1,
+        num_generations=n_generations,
+        k=0,
+        p=0.75)
+    l = []
+    for i in range(n_generations):
+        l.append(prediction.generations[i].text)
+    return l
+
+while True:
+    line = input()
+    text = to_transcript(line)
+    items = do_cohere(text) + [text]
+    for i in items:
+        print(i)
