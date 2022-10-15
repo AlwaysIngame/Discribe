664fa5936e3b9290e78dd63a8cd904a8c4b598e2
Hriday Algh <hridayalgh@gmail.com>
2022-10-15 19:20:57 -0400

    Profainty filter

--- summarize.py
+++ summarize.py
@@ -1,6 +1,7 @@
 import cohere
 import whisper
 import os
+from better_profanity import profanity
 api_key = 'FRyzo2xU7j2Cp9sPgpSHTafsBsuI3X4UnNmBmDZO'
 co = cohere.Client(api_key)
 model = whisper.load_model("base")
@@ -15,7 +16,7 @@ def do_cohere(prompt):
         n_generations = 4
         prediction = co.generate(
             model='large',
-            prompt=prompt,
+            prompt=profanity.censor(prompt),
             return_likelihoods = 'GENERATION',
             stop_sequences=['"'],
             max_tokens = min(512, max(40, len(prompt)//10)),
@@ -23,10 +24,10 @@ def do_cohere(prompt):
             num_generations=n_generations,
             k=0,
             p=0.75)
-        l = []
+        l = set()
         for i in range(len(prediction.generations)):
-            l.append(prediction.generations[i].text)
-        return l
+            l.add(prediction.generations[i].text)
+        return list(l)
     except cohere.error.CohereError:
         return ["No bad words in summarization :bangbang:"]
 
@@ -35,8 +36,8 @@ while True:
     line = input()
     t = to_transcript(line)
     items = do_cohere(t)
-    for i in items:
-        print("**:arrow_down: Summarization " + str(items.index(i) + 1) + ":arrow_down:**")
-        print(i)
+    for i in range(len(items)):
+        print("**:arrow_down: Summarization " + str(i + 1) + " :arrow_down:**")
+        print(items[i])
     os.remove(line.strip())
     continue
