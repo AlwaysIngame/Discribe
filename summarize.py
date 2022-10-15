3c991adb7f6b4fbcac075e8b8eb99500aa110c28
Hriday Algh <hridayalgh@gmail.com>
2022-10-15 18:44:34 -0400

    block bad words

--- summarize.py
+++ summarize.py
@@ -11,31 +11,32 @@ def to_transcript(filename):
     return result["text"]
 
 def do_cohere(prompt):
-    n_generations = 4
-    prediction = co.generate(
-        model='large',
-        prompt=prompt,
-        return_likelihoods = 'GENERATION',
-        stop_sequences=['"'],
-        max_tokens = min(512, max(40, len(prompt)//10)),
-        temperature=1,
-        num_generations=n_generations,
-        k=0,
-        p=0.75)
-    l = []
-    for i in range(len(prediction.generations)):
-        l.append(prediction.generations[i].text)
-    return l
+    try:
+        n_generations = 4
+        prediction = co.generate(
+            model='large',
+            prompt=prompt,
+            return_likelihoods = 'GENERATION',
+            stop_sequences=['"'],
+            max_tokens = min(512, max(40, len(prompt)//10)),
+            temperature=1,
+            num_generations=n_generations,
+            k=0,
+            p=0.75)
+        l = []
+        for i in range(len(prediction.generations)):
+            l.append(prediction.generations[i].text)
+        return l
+    except cohere.error.CohereError:
+        return ["No bad words in summarization :bangbang:"]
+
 
 while True:
-    try:
-        line = input()
-        t = to_transcript(line)
-        items = do_cohere(t)
-        for i in items:
-            print("**:arrow_down: Summarization " + str(items.index(i) + 1) + ":arrow_down:**")
-            print(i)
-        os.remove(line.strip())
-    except cohere.error.CohereError:
-        print("The summarization failed. :sob:")
-        continue
+    line = input()
+    t = to_transcript(line)
+    items = do_cohere(t)
+    for i in items:
+        print("**:arrow_down: Summarization " + str(items.index(i) + 1) + ":arrow_down:**")
+        print(i)
+    os.remove(line.strip())
+    continue
