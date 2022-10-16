1383e8f986c1234da52b515119aa3d0025ccb158
Hriday Algh <hridayalgh@gmail.com>
2022-10-15 20:31:27 -0400

    Adjust Cohere values

--- summarize.py
+++ summarize.py
@@ -12,6 +12,7 @@ currtime = None
 
 def to_transcript(filename):
     result = model.transcribe(filename.strip())
+    os.remove(filename.strip())
     print(
         "**:arrow_down: Full Transcript from call at "
         + currtime
@@ -29,15 +30,16 @@ def do_cohere(prompt):
             prompt=prompt,
             return_likelihoods="GENERATION",
             stop_sequences=['"'],
-            max_tokens=min(512, max(40, len(prompt) // 10)),
-            temperature=1,
+            max_tokens=min(512, max(40, len(prompt) // 20)),
+            temperature=0.6,
             num_generations=n_generations,
             k=0,
             p=0.75,
         )
         l = set()
-        for i in range(len(prediction.generations)):
-            l.add(prediction.generations[i].text)
+        for i in prediction.generations:
+            if len(i.text) < len(prompt):
+                l.add(i.text)
         return list(l)
     except cohere.error.CohereError:
         return ["No bad words in summarization :bangbang:"]
@@ -51,5 +53,3 @@ while True:
     for i in range(len(items)):
         print("**:arrow_down: Summarization " + str(i + 1) + " :arrow_down:**")
         print(items[i])
-    os.remove(line.strip())
-    continue
